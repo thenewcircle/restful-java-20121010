5a9dcc3381ba4324699c32babd668606688cee74
Dan Rosen <dan@marakana.com>
2012-10-10 15:29:43 -0700

    added sub-resource to hello

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,12 +2,19 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
-@Path("/")
+@Path("/greeting")
 public class HelloResource {
 
 	@GET
 	public String getHello() {
 		return "Hello!";
+	}
+
+	@GET
+	@Path("{name}")
+	public String getHelloSomebody(@PathParam("name") String name) {
+		return "Hello, " + name + "!";
 	}
 }
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -4,12 +4,22 @@ import static junit.framework.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.sun.jersey.api.client.WebResource;
+
 public class HelloResourceTest extends ResourceTest {
+
+	private final WebResource greeting = resource().path("greeting");
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = resource().path("/").get(String.class);
+		String hello = greeting.get(String.class);
 		assertEquals("Hello!", hello);
+	}
+
+	@Test
+	public void helloSubresourceMustSayHello() {
+		String hello = greeting.path("Cisco").get(String.class);
+		assertEquals("Hello, Cisco!", hello);
 	}
 
 }
