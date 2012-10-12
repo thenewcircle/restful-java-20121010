ecfb78d677d25b7f29fd306b04ec227409b69a42
Dan Rosen <dan@marakana.com>
2012-10-12 10:44:30 -0700

    use declarative linking for structural and transitional links, for UserRepresentation

--- src/main/java/chirp/service/representations/UserRepresentation.java
+++ src/main/java/chirp/service/representations/UserRepresentation.java
@@ -2,46 +2,44 @@ package chirp.service.representations;
 
 import java.net.URI;
 
-import javax.ws.rs.core.UriBuilder;
-
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import chirp.model.User;
-import chirp.service.resources.PostsResource;
-import chirp.service.resources.UsersResource;
 
+import com.sun.jersey.server.linking.Link;
+import com.sun.jersey.server.linking.Links;
+import com.sun.jersey.server.linking.Ref;
+
+@Links({
+	@Link(value = @Ref("/users/{username}"), rel = "self"),
+	@Link(value = @Ref("/users"), rel = "up"),
+	@Link(value = @Ref("/posts/{username}"), rel = "related")
+})
 public class UserRepresentation {
 
-	private final URI self;
-	private final URI posts;
+	@Ref("/users/{username}")
+	private URI self;
+
 	private final String username;
 	private final String realname;
 
 	public UserRepresentation(User user, boolean summary) {
-		username = summary ? null : user.getUsername();
+		username = user.getUsername();
 		realname = summary ? null : user.getRealname();
-		self = UriBuilder.fromResource(UsersResource.class).path(user.getUsername()).build();
-		posts = summary ? null : UriBuilder.fromResource(PostsResource.class).build(username);
 	}
 
 	@JsonCreator
 	public UserRepresentation(@JsonProperty("self") URI self,
-			@JsonProperty("posts") URI posts,
 			@JsonProperty("username") String username,
 			@JsonProperty("realname") String realname) {
 		this.self = self;
-		this.posts = posts;
 		this.username = username;
 		this.realname = realname;
 	}
 
 	public URI getSelf() {
 		return self;
-	}
-
-	public URI getPosts() {
-		return posts;
 	}
 
 	public String getUsername() {
