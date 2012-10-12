a988aca779acc657074a4e8d0fc22caab5d196d0
Dan Rosen <dan@marakana.com>
2012-10-12 12:44:44 -0700

    implemented conditional requests based on entity tags

--- src/main/java/chirp/service/resources/PostsResource.java
+++ src/main/java/chirp/service/resources/PostsResource.java
@@ -1,6 +1,7 @@
 package chirp.service.resources;
 
 import java.net.URI;
+import java.util.Collection;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -8,8 +9,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriBuilder;
 
 import chirp.model.Post;
@@ -40,8 +45,17 @@ public class PostsResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public PostCollectionRepresentation getPosts(@PathParam("username") String username) {
-		return new PostCollectionRepresentation(userRepository.getUser(username).getPosts(), username);
+	public Response getPosts(@PathParam("username") String username, @Context Request request) {
+		Collection<Post> posts = userRepository.getUser(username).getPosts();
+
+		EntityTag eTag = new EntityTag(String.valueOf(posts.hashCode()));
+		ResponseBuilder response = request.evaluatePreconditions(eTag);
+		if (response != null) {
+			return response.build();
+		}
+
+		PostCollectionRepresentation representation = new PostCollectionRepresentation(posts, username);
+		return Response.ok(representation).tag(eTag).build();
 	}
 
 	@GET
--- src/main/java/chirp/service/resources/UsersResource.java
+++ src/main/java/chirp/service/resources/UsersResource.java
@@ -1,6 +1,7 @@
 package chirp.service.resources;
 
 import java.net.URI;
+import java.util.Collection;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -8,8 +9,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriBuilder;
 
 import chirp.model.User;
@@ -40,8 +45,17 @@ public class UsersResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public UserCollectionRepresentation getUsers() {
-		return new UserCollectionRepresentation(userRepository.getUsers());
+	public Response getUsers(@Context Request request) {
+		Collection<User> users = userRepository.getUsers();
+
+		EntityTag eTag = new EntityTag(String.valueOf(users.hashCode()));
+		ResponseBuilder response = request.evaluatePreconditions(eTag);
+		if (response != null) {
+			return response.build();
+		}
+
+		UserCollectionRepresentation representation = new UserCollectionRepresentation(users);
+		return Response.ok(representation).tag(eTag).build();
 	}
 
 	@GET
